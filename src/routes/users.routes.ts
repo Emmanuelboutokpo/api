c4d7d4179c634438275d155f6f2a41e44423c0ff
Emmanuelboutokpo <juliospoisson@gmail.com>
2025-10-21 17:11:21 +0100

    add user/me protected route fixed

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { getMe, getUsers, updateUser } from '../controllers/users.controller';
+import { getMe, getUser, getUsers, updateUser } from '../controllers/users.controller';
 
 import type { Router as ExpressRouter } from 'express';
 import { requireAuth } from '@clerk/express';
@@ -9,7 +9,7 @@ const router: ExpressRouter = Router();
 
 // Routes
   router.get('/me', requireAuth(), getMe );
-  router.get('/user/me', requireAuth(), getMe );
+  router.get('/user/me', requireAuth(), getUser );
   router.get('/users', requireAuth(), getUsers);
   router.patch('/users/:userId', requireAuth(), updateUser);  
 

f55b4b5fd96c438cfe31f25f208784a774db83c8
Emmanuelboutokpo <juliospoisson@gmail.com>
2025-12-15 21:40:49 +0100

    update resend-opt

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 const router: Router = Router();
 
-import { signUpEmail, verifyOtp, logoutUser, refreshTokens, login } from '../controllers/auth.controller';
+import { signUpEmail, verifyOtp, logoutUser, refreshTokens, login, resendOtp } from '../controllers/auth.controller';
 import { isRequestValidated } from '../validator/auth.validator';
 
 router.post('/signup', signUpEmail, isRequestValidated);    
 router.post('/verify-otp', verifyOtp);
-router.post('/resend-otp', verifyOtp);
+router.post('/resend-otp', resendOtp);
 router.post('/login', login, isRequestValidated);
 router.post('/refresh-token', refreshTokens);
 router.post('/logout/:id', logoutUser);

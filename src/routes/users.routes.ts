ab361e2747e04cebc17f79c1aac15e77acefbb08
Emmanuelboutokpo <juliospoisson@gmail.com>
2026-01-15 12:24:07 +0100

    update user get controller

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { deleteUser, getEmployeesAndControleurs, getUser, getUsers, updateUser } from '../controllers/users.controller';
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/users.controller';
 
 import type { Router as ExpressRouter } from 'express';
 const router: ExpressRouter = Router();
@@ -8,7 +8,6 @@ const router: ExpressRouter = Router();
 
 // Routes
   router.get('/user/me/:id', getUser);
-  router.get('/users/team', getEmployeesAndControleurs);
   router.get('/users', getUsers);
   router.put('/users/:id', updateUser);
   router.delete('/users/:id', deleteUser);
